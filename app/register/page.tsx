912ab27ac73181a5cf37c6949a405d98c3ba0382
Tahsin Özgür KOÇ <tahsinkoc6@gmail.com>
2024-05-18 20:06:59 +0300

    login register pages ui done

---
+++ app/register/page.tsx
@@ -0,0 +1,14 @@
+import Register from '@/components/Register'
+import React from 'react'
+
+type Props = {}
+
+function page({ }: Props) {
+    return (
+        <div>
+            <Register />
+        </div>
+    )
+}
+
+export default page
